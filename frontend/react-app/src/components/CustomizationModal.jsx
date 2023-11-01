ac2cf5ba9991a5cc68445df951ba0a7958e8af9c
ambercj <98664147+ambercj@users.noreply.github.com>
2023-11-01 14:00:21 -0500

    customize buttons added

--- frontend/react-app/src/components/CustomizationModal.jsx
+++ frontend/react-app/src/components/CustomizationModal.jsx
@@ -8,6 +8,46 @@ const CustomizationModal = ({ drink, onClose }) => {
         <button className="close-button" onClick={onClose}>X</button>
         <h2>{drink.name}</h2>
         {/* Your customization options go here */}
+
+        <div className="ice-buttons-container">
+          <h3>Ice Level</h3>
+          <button>Normal Ice</button>
+          <button>Less Ice</button>
+          <button>No Ice</button>
+          <br></br>
+          <button>Extra Ice</button>
+        </div>
+
+        <div className="sweetness-buttons-container">
+          <h3>Sweetness Level</h3>
+          <button>100%</button>
+          <button>80%</button>
+          <button>50%</button>
+          <br></br>
+          <button>30%</button>
+          <button>0%</button>
+          <button>120%</button>
+        </div>
+
+        <div className="toppings-buttons-container">
+          <h3>Toppings</h3>
+          <button>Pearl</button>
+          <button>Mini Pearl</button>
+          <button>Ice Cream</button>
+          <br></br>
+          <button>Pudding</button>
+          <button>Aloe Vera</button>
+          <button>Red Bean</button>
+          <br></br>
+          <button>Herb Jelly</button>
+          <button>Aiyu Jelly</button>
+          <button>Lychee Jelly</button>
+          <br></br>
+          <button>Crystal Boba</button>
+          <button>Creama</button>
+        </div>
+
+        <br></br>
         <button onClick={onClose}>Done</button>
       </div>
     </div>
