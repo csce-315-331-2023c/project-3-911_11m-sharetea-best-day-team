d7a36abc58c196b73231420fc7fa582830aabd71
ambercj <98664147+ambercj@users.noreply.github.com>
2023-11-01 23:56:44 -0500

    fixed layout for customize popup

--- frontend/react-app/src/components/CustomizationModal.jsx
+++ frontend/react-app/src/components/CustomizationModal.jsx
@@ -5,49 +5,80 @@ const CustomizationModal = ({ drink, onClose }) => {
   return (
     <div className="modal-background" onClick={onClose}>
       <div className="modal-content" onClick={e => e.stopPropagation()}>
-        <button className="close-button" onClick={onClose}>X</button>
+        <button className="close-button" onClick={onClose}>x</button>
+        <div>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+        </div>
         <h2>{drink.name}</h2>
-        {/* Your customization options go here */}
+        
+        <div className="button-section">
+          <div className="ice-buttons-container">
+            <div className="container-row">
+              <h3>Ice Level</h3>
+            </div>
+            <div className="container-row">
+              <button>Normal Ice</button>
+              <button>Less Ice</button>
+              <button>No Ice</button>
+            </div>
+            <div className="container-row">
+              <button>Extra Ice</button>
+            </div>
+          </div>
 
-        <div className="ice-buttons-container">
-          <h3>Ice Level</h3>
-          <button>Normal Ice</button>
-          <button>Less Ice</button>
-          <button>No Ice</button>
+          <div className="sweetness-buttons-container">
+            <div className="container-row">
+              <h3>Sweetness Level</h3>
+            </div>
+            <div className="container-row">
+              <button>100%</button>
+              <button>80%</button>
+              <button>50%</button>
+            </div>
+            <div className="container-row">
+              <button>30%</button>
+              <button>0%</button>
+              <button>120%</button>
+            </div>
+          </div>
+
+          <div className="toppings-buttons-container">
+            <div className="container-row">
+              <h3>Toppings</h3>
+            </div>
+            <div className="container-row">
+              <button>Pearl</button>
+              <button>Mini Pearl</button>
+              <button>Ice Cream</button>
+            </div>
+            <div className="container-row">
+              <button>Pudding</button>
+              <button>Aloe Vera</button>
+              <button>Red Bean</button>
+            </div>
+            <div className="container-row">
+              <button>Herb Jelly</button>
+              <button>Aiyu Jelly</button>
+              <button>Lychee Jelly</button>
+            </div>
+            <div className="container-row">
+              <button>Crystal Boba</button>
+              <button>Creama</button>
+            </div>
+          </div>
+
           <br></br>
-          <button>Extra Ice</button>
+
         </div>
 
-        <div className="sweetness-buttons-container">
-          <h3>Sweetness Level</h3>
-          <button>100%</button>
-          <button>80%</button>
-          <button>50%</button>
-          <br></br>
-          <button>30%</button>
-          <button>0%</button>
-          <button>120%</button>
-        </div>
-
-        <div className="toppings-buttons-container">
-          <h3>Toppings</h3>
-          <button>Pearl</button>
-          <button>Mini Pearl</button>
-          <button>Ice Cream</button>
-          <br></br>
-          <button>Pudding</button>
-          <button>Aloe Vera</button>
-          <button>Red Bean</button>
-          <br></br>
-          <button>Herb Jelly</button>
-          <button>Aiyu Jelly</button>
-          <button>Lychee Jelly</button>
-          <br></br>
-          <button>Crystal Boba</button>
-          <button>Creama</button>
-        </div>
-
-        <br></br>
         <button onClick={onClose}>Done</button>
       </div>
     </div>
