0319419277c0d0ec34cff87d20e1f43a84170d73
seanacaballa <seanacaballa@gmail.com>
2023-11-03 08:18:50 -0500

    small changes to home structure

--- frontend/react-app/src/pages/Home.js
+++ frontend/react-app/src/pages/Home.js
@@ -18,11 +18,12 @@ export function Home() {
 
         <hr className="red-horizontal-rule"></hr>
 
-        Lorem ipsum dolor sit amet. Eos velit velit et voluptate error aut itaque laboriosam qui corporis magni eum porro nihil. Et temporibus voluptatem qui perspiciatis sint aut nesciunt deleniti in recusandae voluptatem ut inventore nemo aut corporis quia?
+        <p>Lorem ipsum dolor sit amet. Eos velit velit et voluptate error aut itaque laboriosam qui corporis magni eum porro nihil. Et temporibus voluptatem qui perspiciatis sint aut nesciunt deleniti in recusandae voluptatem ut inventore nemo aut corporis quia?
 
 Hic minima corporis sed perspiciatis expedita 33 eaque omnis et voluptatem voluptatibus. Eum pariatur necessitatibus aut temporibus error eos officiis totam ut laborum quidem. Et accusamus voluptas aut provident aliquam et voluptatibus incidunt sed necessitatibus quia qui odio corrupti. Cum doloribus error a quia ipsam est aspernatur consequatur.
 
 Qui saepe voluptatibus et natus harum qui exercitationem ipsum ut recusandae minima ut debitis Quis? Ut mollitia nisi est quasi molestias ea dolor quia ut dignissimos quas id exercitationem magni et reiciendis consequatur qui voluptatem dolor. Hic perspiciatis iste vel facilis quia id optio quod. Sed laboriosam eius in cupiditate possimus quo pariatur doloribus et sunt repellendus sit repudiandae totam.
+</p>
 
         {/* <footer> */}
         <div className="footer-container">
