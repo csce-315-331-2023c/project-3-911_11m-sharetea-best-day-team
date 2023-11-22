0f804a057c0c48770df4897dd269d4c39e4050d4
Tommy Zheng <tommyztx@gmail.com>
2023-11-21 19:58:42 -0600

    Delete inventory and comments

--- frontend/react-app/src/components/DatabaseTable.jsx
+++ frontend/react-app/src/components/DatabaseTable.jsx
@@ -18,98 +18,97 @@ import {
   randomId,
 } from '@mui/x-data-grid-generator';
 
+/**
+ * Thomas Zheng
+ * @param {*} query the query to reload the page
+ * @returns Inventory as rows
+ */
 const fetchDataFromQuery = async (query) => {
-    try {
-        const response = await fetch('https://backend-heli.onrender.com/query', {
-          method: 'POST',
-          body: JSON.stringify({ query }),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        });
-        const data = await response.json();
-        return data;
-      } catch (error) {
-        console.error('Error fetching data', error);
+  try {
+    //Make API call
+    const response = await fetch('http://localhost:9000/query', {
+      method: 'POST',
+      body: JSON.stringify({ query }), //Put the query in the body
+      headers: {
+        'Content-Type': 'application/json'
       }
-    };
+    });
+    const data = await response.json(); //retreive response as a json
+    return data;
+  } catch (error) {
+    console.error('Error fetching data', error);
+  }
+};
 
-    const insertDataFromQuery = async (query) => {
-      try {
-          console.log(query);
-          fetch('https://backend-heli.onrender.com/update-data', {
-            method: 'PUT',
-            body: JSON.stringify({ query }),
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          });
-        } catch (error) {
-          console.error('Error fetching data', error);
-        }
-      };
-
-    const isImageUrl = (column) => {
-      if(column === "images"){
-        return true;
+/**
+ * Insert, Update, and Delete API call
+ * @param {*} query 
+ */
+const insertDataFromQuery = async (query) => {
+  try {
+    //Make API call, no output
+    fetch('http://localhost:9000/update-data', {
+      method: 'PUT',
+      body: JSON.stringify({ query }), //Using PUT call
+      headers: {
+        'Content-Type': 'application/json'
       }
-      else {
-        return false;
-      }
-    };
-    
+    });
+  } catch (error) {
+    console.error('Error fetching data', error);
+  }
+}
 
+/**
+ * Database Component for Inventory
+ * @param {*} param0 query to run database component 
+ * @returns table
+ */
 const DatabaseTable = ({ query }) => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [ident, setIDs] = useState();
+  
+  //Props to use
   const [changed, setChanged] = useState(false);
   const [rows, setRows] = useState([]);
   const [rowModesModel, setRowModesModel] = useState({});
   const [pendingSaveRow, setPendingSaveRow] = useState(null);
 
+  /**
+   * Used to Reload the page with the SELECT query
+   */
   useEffect(() => {
     const reloadData = async () => {
-      setLoading(true);
-        setError(null);
-        try {
-          const result = await fetchDataFromQuery(query);
-          //setData(result);
-          const newRows = result.map(row => {
-            return {
-              id:randomId(),
-              ingredient: row.ingredient,
-              count: row.count,
-              min: row.min,
-              isNew: false,
-            };
-          });
-          setRows(newRows);
-          //setQuerys(query);
-        } catch (err) {
-          setError(err.message);
-        } finally {
-          setLoading(false);
-        }
+      try {
+        const result = await fetchDataFromQuery(query);
+        const newRows = result.map(row => { //Each row that is created needs an ID, ingredient, count, min, and isNew to false as they were already in the database
+          return {
+            id:randomId(),
+            ingredient: row.ingredient,
+            count: row.count,
+            min: row.min,
+            isNew: false,
+          };
+        });
+        setRows(newRows); // set the new Rows
+      } catch (err) {
+        console.error('Error fetching data', err);
+      }
     }
 
     reloadData();
-  }, [changed]);
+  }, [changed]); //Reload when changes happen
 
+  /** 
+   * Used to either update or insert when a row is updated, Determined by whether the row isNew or not. New means insert, not New means update
+  */
   useEffect(() => {
     const updateRowInDatabase = async () => {
-      //const updatedRow = rows.find(row => row.id === ident);
       if (pendingSaveRow) {
         try {
-
           if (pendingSaveRow.isNew) {
             await addNewIngredient(pendingSaveRow);
           } else {
-            console.log(pendingSaveRow.count);
             await updateIngredient(pendingSaveRow);
           }
-          //setChanged(!changed);
           // Add logic to re-fetch data or update the state with the new row data
         } catch (error) {
           console.error('Error updating row:', error);
@@ -118,42 +117,56 @@ const DatabaseTable = ({ query }) => {
       }
     };
     updateRowInDatabase();
-    setChanged(!changed);
+    setChanged(!changed); //Reload the page after finished with PUT request
   }, [pendingSaveRow]);
 
+  /**
+   * Update the Ingredient in the database
+   * @param {*} ingredients the new ingredient to update
+   */
   const updateIngredient = async (ingredients) => {
     await insertDataFromQuery(`UPDATE inventory SET ingredient='${ingredients.ingredient}', count=${ingredients.count}, min=${ingredients.min} WHERE ingredient='${ingredients.ingredient}';`);
   };
 
+  /**
+   * Add a new Ingredient to the database
+   * @param {*} ingredients the new ingredient to insert
+   */
   const addNewIngredient = async (ingredients) => {
-    //fetchDataFromQuery(`UPDATE your_table_name SET ingredient='${ingredient.ingredient}', count=${ingredient.count}, min=${ingredient.min} WHERE id=${ingredient.id}`);
     await insertDataFromQuery(`INSERT INTO inventory (ingredient, count, min) VALUES ('${ingredients.ingredient}', ${ingredients.count}, ${ingredients.min});`);
-    //const query = `UPDATE your_table_name SET ingredient='${ingredient.ingredient}', count=${ingredient.count}, min=${ingredient.min} WHERE id=${ingredient.id}`;
-    //setChanged(!changed);
   };
 
+  /**
+   * Delete an ingredient from the database
+   * @param {*} ingredients the ingredient to be deleted
+   */
+  const deleteIngredient = async (ingredients) => {
+    await insertDataFromQuery(`DELETE FROM inventory WHERE ingredient='${ingredients.ingredient}';`);
+  };
+
+  /**
+   * 
+   * @param {*} props Used to change and configure whenever a new recod is added 
+   * @returns 
+   */
   function EditToolbar(props) {
-    const { setRows, setRowModesModel, setIDs} = props;
+    const { setRows, setRowModesModel} = props;
   
     const handleClick = () => {
-      const new_id = randomId();
-      setIDs(new_id);
-      const newIngredient = { id: new_id, ingredient: '', count: '', min: '', isNew: true };
-      setRows((oldRows) => [...oldRows, newIngredient]);
+      const new_id = randomId(); //With a new row, there is a new ID
+      const newIngredient = { id: new_id, ingredient: '', count: '', min: '', isNew: true }; // Define row as New
+      setRows((oldRows) => [...oldRows, newIngredient]); // Add new Row to the table
       setRowModesModel((oldModel) => ({
         ...oldModel,
         [newIngredient.id]: { mode: GridRowModes.Edit, fieldToFocus: 'ingredient' },
-      }));
-      //setChanged(!changed);
-       // Call the function to add the new ingredient
-       //updateIngredient(newIngredient);
-  
+      })); 
     };
   
+    //Button
     return (
       <GridToolbarContainer>
         <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
-          Add record
+          Add Ingredient
         </Button>
       </GridToolbarContainer>
     );
@@ -165,26 +178,25 @@ const DatabaseTable = ({ query }) => {
     }
   };
 
+  //Change row to editable
   const handleEditClick = (id) => () => {
     setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
   };
 
+  //Return Row to Visible
   const handleSaveClick = (id) => () => {
-    const updatedRow = rows.find(row => row.id === id);
-    if (!updatedRow) {
-      console.error('Row not found');
-      return;
-    }
-  
-    setIDs(id);
-    
     setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
   };
 
-  const handleDeleteClick = (id) => () => {
+  //Delete Row
+  const handleDeleteClick = (id) => async () => {
+    const deletingRow = rows.find((row) => row.id == id);
+    await deleteIngredient(deletingRow);
     setRows(rows.filter((row) => row.id !== id));
+
   };
 
+  //Cancel deleting Row
   const handleCancelClick = (id) => () => {
     setRowModesModel({
       ...rowModesModel,
@@ -197,38 +209,33 @@ const DatabaseTable = ({ query }) => {
     }
   };
 
+  //If the processed Row is new, then return isNew, otherwise, update
   const processRowUpdate = (newRow) => {
-    //const updatedRow = newRow;
-    //if(updatedRow.isNew === true) {
-      if(newRow.isNew === true) {
-        setPendingSaveRow(newRow);
-      }
-      console.log(newRow.min);
-      const updatedRow = { ...newRow, isNew: false };
-
-      if(newRow.isNew === false) {
-        setPendingSaveRow(updatedRow);
-      }
-    // } else {
-    //   updatedRow = { ...newRow, isNew: false };
-    //   setPendingSaveRow(updatedRow);
-    // }
+    if(newRow.isNew === true) {
+      setPendingSaveRow(newRow);
+    }
+    const updatedRow = { ...newRow, isNew: false };
+    if(newRow.isNew === false) {
+      setPendingSaveRow(updatedRow);
+    }
     setChanged(!changed);
     return updatedRow;
   };
 
+  //Handle the row model change
   const handleRowModesModelChange = (newRowModesModel) => {
     setRowModesModel(newRowModesModel);
   };
   
 
+  //Columns for the table
   const columns = [
-    { field: 'ingredient', headerName: 'ingredient', width: 180, editable: true },
+    { field: 'ingredient', headerName: 'ingredient', width: 300, editable: true },
     {
       field: 'count',
       headerName: 'count',
       type: 'number',
-      width: 80,
+      width: 250,
       align: 'left',
       headerAlign: 'left',
       editable: true,
@@ -237,14 +244,16 @@ const DatabaseTable = ({ query }) => {
       field: 'min',
       headerName: 'min',
       type: 'number',
-      width: 180,
+      width: 200,
+      align: 'left',
+      headerAlign: 'left',
       editable: true,
     },
     {
       field: 'actions',
       type: 'actions',
       headerName: 'Actions',
-      width: 100,
+      width: 200,
       cellClassName: 'actions',
       getActions: ({ id }) => {
         const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
@@ -289,6 +298,7 @@ const DatabaseTable = ({ query }) => {
   ];
 
 
+  //Return
   return (
     <Box
       sx={{
@@ -314,7 +324,7 @@ const DatabaseTable = ({ query }) => {
           toolbar: EditToolbar,
         }}
         slotProps={{
-          toolbar: { setRows, setRowModesModel, setIDs},
+          toolbar: { setRows, setRowModesModel},
         }}
       />
     </Box>
