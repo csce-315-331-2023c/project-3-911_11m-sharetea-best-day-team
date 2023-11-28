ce070266449b1e5fc152cd13acc45b870c1ec422
ambercj <98664147+ambercj@users.noreply.github.com>
2023-11-28 13:32:43 -0600

    added translation feature

--- frontend/react-app/src/components/AccessibilityButton.jsx
+++ frontend/react-app/src/components/AccessibilityButton.jsx
@@ -1,62 +1,70 @@
-import React, { useState } from 'react';
-import Picture from '../images/accessibility-icon.png'
-import '../styles/general.css'
+import React, { useState, useEffect } from 'react';
+import Picture from '../images/accessibility-icon.png';
+import '../styles/general.css';
 
 const AccessibilityButton = () => {
-    const [showPopup, setShowPopup] = useState(false);
-    const [isPaused, setIsPaused] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
 
-    const handleButtonClick = () => {
-        setShowPopup(!showPopup);
+  const handleButtonClick = () => {
+    setShowPopup(!showPopup);
+  };
+
+  const handleReadPageClick = () => {
+    const textToRead = document.body.innerText;
+    const speech = new SpeechSynthesisUtterance(textToRead);
+    window.speechSynthesis.speak(speech);
+  };
+
+  const handleStopReadingClick = () => {
+    window.speechSynthesis.cancel();
+  };
+
+  const handlePauseContinueClick = () => {
+    if (isPaused) {
+      window.speechSynthesis.resume();
+      setIsPaused(false);
+    } else {
+      window.speechSynthesis.pause();
+      setIsPaused(true);
     }
+  };
 
-    const handleReadPageClick = () => {
-        const textToRead = document.body.innerText;
-        const speech = new SpeechSynthesisUtterance(textToRead);
-        window.speechSynthesis.speak(speech);
-    }
+  useEffect(() => {
+    // Load the Google Translate script
+    const script = document.createElement('script');
+    script.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+    script.async = true;
+    document.head.appendChild(script);
 
-    const handleStopReadingClick = () => {
-        window.speechSynthesis.cancel();
-    }
+    // Initialize the translation element when the script is loaded
+    window.googleTranslateElementInit = function () {
+      new window.google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
+    };
 
-    const handlePauseContinueClick = () => {
-        if (isPaused) {
-            window.speechSynthesis.resume();
-            setIsPaused(false);
-        } else {
-            window.speechSynthesis.pause();
-            setIsPaused(true);
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className='accessibility-container'>
+        <img src={Picture} className='accessibility-button' onClick={handleButtonClick} alt="Accessibility Button" />
+        {showPopup && 
+          <div className='translation-popup'>
+            {/* <button onClick={() => console.log('Translate to Spanish')}>Translate to Spanish</button>
+            <button onClick={() => console.log('Translate to French')}>Translate to French</button>
+            <button onClick={() => console.log('Translate to German')}>Translate to German</button> */}
+            <button onClick={handleReadPageClick}>Read Page</button>
+            <button onClick={handleStopReadingClick}>Stop Reading</button>
+            <button onClick={handlePauseContinueClick}>{isPaused ? 'Continue Reading' : 'Pause Reading'}</button>
+            {/* Add more translation options as needed */}
+          </div>
         }
-    }
-
-    return (
-        <>
-        <div className='accessibility-container'>
-            <img src={Picture} className='accessibility-button' onClick={handleButtonClick}></img>
-            {showPopup && 
-                <div className='translation-popup'>
-                    <button onClick={() => console.log('Translate to Spanish')}>Translate to Spanish</button>
-                    <button onClick={() => console.log('Translate to French')}>Translate to French</button>
-                    <button onClick={() => console.log('Translate to German')}>Translate to German</button>
-                    <button onClick={handleReadPageClick}>Read Page</button>
-                    <button onClick={handleStopReadingClick}>Stop Reading</button>
-                    <button onClick={handlePauseContinueClick}>{isPaused ? 'Continue Reading' : 'Pause Reading'}</button>
-                    {/* Add more translation options as needed */}
-                </div>
-            }
-        </div>
-        {/* <script src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-        <script>
-            function googleTranslateElementInit(){
-                new google.translate.TranslateElement(
-                    {pageLanguage: 'en'},
-                    'google_translate_element'
-                )
-            };
-        </script> */}
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default AccessibilityButton;
